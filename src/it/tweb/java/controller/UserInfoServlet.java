c7d9529e61e5806f21434d7f244651a8ecad219e
Giuseppe Nesca <giuseppenesca.g@gmail.com>
2019-03-18 14:39:12 +0100

    userinfoServlet added

---
+++ src/it/tweb/java/controller/UserInfoServlet.java
@@ -0,0 +1,37 @@
+package it.tweb.java.controller;
+
+import com.google.gson.Gson;
+import it.tweb.java.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "UserInfoServlet", value = "/userinfo")
+public class UserInfoServlet extends HttpServlet {
+    private Gson gson = new Gson();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        handleRequest(request, response);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        handleRequest(request, response);
+    }
+
+    private void handleRequest (HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        User user = null;
+        if(session != null) {
+            user = (User) session.getAttribute("user");
+        }
+
+        if (session == null || user == null) {
+            response.setStatus(401);
+        } else {
+            response.getWriter().write(gson.toJson(user)); //200, ok
+        }
+    }
+}
