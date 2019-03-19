b9d9130edfc6e6504a30347328ce0e07ae4d5ea1
Giuseppe Nesca <giuseppenesca.g@gmail.com>
2019-03-19 13:21:46 +0100

    login on get

--- src/it/tweb/java/controller/LoginServlet.java
+++ src/it/tweb/java/controller/LoginServlet.java
@@ -20,7 +20,7 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        handleLogin(request, response);
     }
 
     @Override
