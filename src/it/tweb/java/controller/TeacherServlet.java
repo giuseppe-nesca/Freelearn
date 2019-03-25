64485545409d5c8eddda7fa9f2fa414407aeaa15
Giuseppe Nesca <giuseppenesca.g@gmail.com>
2019-03-25 13:18:07 +0100

    TEACHERSERLVET WIP

---
+++ src/it/tweb/java/controller/TeacherServlet.java
@@ -0,0 +1,70 @@
+package it.tweb.java.controller;
+
+import com.google.gson.Gson;
+import it.tweb.java.dao.ManagerDAO;
+import it.tweb.java.dao.SubjectDAO;
+import it.tweb.java.model.Teacher;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import static it.tweb.java.dao.SubjectDAO.getSubjectTeachersByID;
+import static it.tweb.java.utils.ResponseUtils.handleCrossOrigin;
+
+@WebServlet(name = "TeacherServlet", value = "/teachers/getTeacher")
+public class TeacherServlet extends HttpServlet {
+    private Gson gson =  new Gson();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        handleRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.setStatus(401);
+        handleRequest(request, response);
+    }
+
+    private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        handleCrossOrigin(response);
+
+/*        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(401);
+            return;
+        }*/
+
+        String subjectID = request.getParameter("subjectID");
+
+        if (subjectID != null) {
+            int subject = -1;
+            try {
+                subject = Integer.parseInt(subjectID);
+            } catch (NumberFormatException e) {
+                response.setStatus(400);
+                return;
+            }
+            try {
+                List<Teacher> teachers =  SubjectDAO.getSubjectTeachersByID(subject);
+                response.getWriter().write(gson.toJson(teachers));
+                return;
+            } catch (SQLException e) {
+                response.setStatus(503);
+                return;
+            }
+        }
+        response.setStatus(400);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ManagerDAO.registerDriver();
+    }
+}
--- src/it/tweb/java/dao/SubjectDAO.java
+++ src/it/tweb/java/dao/SubjectDAO.java
@@ -1,6 +1,7 @@
 package it.tweb.java.dao;
 
 import it.tweb.java.model.Subject;
+import it.tweb.java.model.Teacher;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,6 +12,14 @@ import java.util.List;
 
 public class SubjectDAO {
     private static final String sql_getSubjects = "SELECT * FROM Subjects;";
+    private static  final String getSql_getSubjectTeachers =
+            "SELECT " +
+                    " courses.id as courseID, " +
+                    " Teachers.name as teacherName, " +
+                    " Teachers.surname as teacherSurname, " +
+                    " teacherID, subjectID " +
+                    " FROM Teachers, courses, subjects " +
+                    " WHERE subjects.id = ? AND courses.subjectID = subjects.id AND courses.teacherID = teachers.id";
 
     static public List<Subject> getLessons() throws SQLException {
         List<Subject> subjects = new ArrayList<>();
@@ -33,4 +42,28 @@ public class SubjectDAO {
         return subjects;
     }
 
+    static public List<Teacher> getSubjectTeachersByID(int id) throws SQLException {
+        List<Teacher> teachers = new ArrayList<>();
+        Connection connection = ManagerDAO.connect();
+        if (connection != null) {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(getSql_getSubjectTeachers);
+                preparedStatement.setInt(1, id);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    int courseID = resultSet.getInt("courseID");
+                    int teacherID = resultSet.getInt("teacherID");
+                    int subjectID = resultSet.getInt("subjectID");
+                    String teacherName = resultSet.getString("teacherName");
+                    String teacherSurname = resultSet.getString("teacherSurname");
+                    teachers.add(new Teacher(teacherID, teacherName, teacherSurname));
+                }
+            } catch (SQLException e) {
+                e.getMessage();
+            } finally {
+                ManagerDAO.disconnect(connection);
+            }
+        } else throw new SQLException();
+        return teachers;
+    }
 }
